994136cd7288888f14452ffd82fdd3204695b50a
Mike Starr <imhs@protonmail.com>
2022-07-08 16:31:47 -0400

    Added media queries to Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,19 +1,35 @@
 // import { ConnectButton } from '@rainbow-me/rainbowkit'
-import { Flex } from '@chakra-ui/react'
+import { Flex, useMediaQuery } from '@chakra-ui/react'
 import SchoolOfCodeLogo from './SchoolOfCodeLogo'
 import HamburgerIcon from './HamburgerIcon'
 import SideDrawer from './SideDrawer'
 
 export default function Header() {
+  const [mobile, desktop] = useMediaQuery([
+    '(min-width: 425px)',
+    '(min-width: 768px)',
+  ])
+
   return (
-    <Flex display="flex" direction="row" align="end" justify="space-between">
-      {/* <SchoolOfCodeLogo autoStart={true} loop={true} /> */}
-      <img
-        src="./assets/logo.svg"
-        alt="Developer DAO School of Code logo"
-        width="37%"
-      />
-      <SideDrawer />
+    <Flex
+      display="flex"
+      direction="row"
+      align="end"
+      justify="space-between"
+      alignItems="center"
+    >
+      {!mobile ? (
+        <>
+          <img
+            src="./assets/logo.svg"
+            alt="Developer DAO School of Code logo"
+            width="37%"
+          />
+          <SideDrawer />
+        </>
+      ) : (
+        <SchoolOfCodeLogo autoStart={true} loop={true} />
+      )}
       {/* <ConnectButton chainStatus="icon" showBalance={false} /> */}
     </Flex>
   )
