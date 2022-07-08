fb85cf0bbb52a2935fc62ff86e3fa18c22e42239
Mike Starr <imhs@protonmail.com>
2022-07-07 22:32:41 -0400

    Created logo.tsx component, added svg image code, imported SVGComponent into Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,13 @@
 // import { ConnectButton } from '@rainbow-me/rainbowkit'
 import { Flex } from '@chakra-ui/react'
 import SchoolOfCodeLogo from './SchoolOfCodeLogo'
+import SVGComponent from './logo'
 
 export default function Header() {
   return (
     <Flex display="flex" direction="row" align="end" justify="space-between">
-      <SchoolOfCodeLogo autoStart={true} loop={true} />
+      {/* <SchoolOfCodeLogo autoStart={true} loop={true} /> */}
+      <SVGComponent />
       {/* <ConnectButton chainStatus="icon" showBalance={false} /> */}
     </Flex>
   )
